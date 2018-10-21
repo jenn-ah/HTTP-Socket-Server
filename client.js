3fba15ef6618ef877ccdb6e2bd7f6533a085d511
JennAh <jennah@Jenns-Air.home>
2018-10-20 16:22:57 -1000

    first header request working

--- client.js
+++ client.js
@@ -8,22 +8,19 @@ const host = process.argv[2];
 const client = net.createConnection(myPort, host, () => {
   console.log('server is connected');
 });
-client.setEncoding('utf8');
-
-
 let date = new Date();
 let rfcDate = date.toUTCString();
-
-console.log(rfcDate);
-
 let uriVar = '/';
 let getHead = 
   `GET ${uriVar} HTTP/1.1 200 OK\n 
   Date: ${rfcDate}\n 
-  ${host}\n 
+  Host: ${host}\n 
   User-Agent: LEJENND 1.0\r\n\r\n`;
 client.write(getHead);
 console.log(getHead);
+
+
+
 
 client.on('data', (data) => {
   console.log('this is my data', data);
